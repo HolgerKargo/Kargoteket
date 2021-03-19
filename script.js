4219240a97b7f82d4d04532fff8c9303a5e9cc61
Holger Kargo <holger.kargo@gmail.com>
2021-03-19 09:22:35 +0100

    Et stort tekststykke i all_else er lavet. Teksten kan kollapses

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+/* Sticky navigation bar */ 
+
 window.onscroll = function() {myFunction()};
 
 var navbar = document.getElementById("navbar");
@@ -9,4 +11,21 @@ function myFunction() {
   } else {
     navbar.classList.remove("sticky");
   }
+}
+
+/* Collapsible text */
+
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
 }
