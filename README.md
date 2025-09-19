a158524572917bab8cbd5a81cc93084a608a332f
Holger Kargo Mortensen <holgerkargo@MacBook-Air-tilhrende-Holger.local>
2025-09-19 14:34:15 +0200

    Initial commit (importeret fra gammel harddisk)

---
+++ README.md
@@ -0,0 +1,24 @@
+# Kargoteket
+
+Dette er skelettet til Dorthe Kargos hjemmeside; Kargoteket.
+
+-
+
+Se videon af Dev Ed.
+
+-
+
+To-do liste:
+
+* 	Burgermenu
+
+*   Responsive (vigtigt!)
+
+*   LinkedIn billede
+
+*   Burgermenu: automatisk lukning af den
+
+*   30px lineheight  i alle
+
+
+_Lavet af Jakob Kargo Mortensen og Holger Kargo Mortensen._
---
+++ script.js
@@ -0,0 +1,33 @@
+/* Sticky navigation bar */ 
+
+window.onscroll = function() {myFunction()};
+
+var navbar = document.getElementById("navbar");
+var sticky = navbar.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
+
+/* Collapsible text */
+
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+      document.getElementById("button-text").innerHTML = "Læs mere her."
+    } else {
+      content.style.display = "block";
+      document.getElementById("button-text").innerHTML = "Læs mindre."
+    }
+  });
+}
